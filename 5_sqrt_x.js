d1c1cecfc89ae6fb9874ea5a7443c3e38aa52ee6
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-07 11:13:34 -0700

    leetcode 5 solution

--- 5_sqrt_x.js
+++ 5_sqrt_x.js
@@ -17,6 +17,20 @@
 // Input: x = 8
 // Output: 2
 // Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+//P We are given a non negative integer
+
+//R: we are returning the floor of the square root of said integer
+
+//E: so if we do 8, we want to return 2 since sqrt(8) = 2.82842
 var mySqrt = function(x) {
-    
+    //P: We want to do a while loop starting at 0
+    //Each while loop will check to see if integer i * i < x
+    //If i * i > x, that means we have gone past what the sqrt of x would be 
+    //that means i is the floor of the sqrt of x
+    let i = 0
+    while (i*i <= x){
+        i+= 1
+    }
+    return i -1
 };
